4bba8d415303b88782b0b4c01353c452ce3f1950
KandrathiVijayalakshmi <vijayalakshmikandrathi1122@gmail.com>
2026-02-11 09:43:08 +0000

    Add files via upload

---
+++ LoginPage.spec.js
@@ -0,0 +1,10 @@
+import { test,expect } from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage'
+test('test',async({page})=>{
+
+    const login = new LoginPage(page)
+
+    await login.gotoLoginPage()
+    await login.login('tomsmith','SuperSecretPassword!')
+
+});
