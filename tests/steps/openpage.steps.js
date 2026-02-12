bb55d40dd71e7e1a459f694e0f382b99d35dbf32
KandrathiVijayalakshmi <vijayalakshmikandrathi1122@gmail.com>
2026-02-12 09:01:38 +0000

    Add files via upload

--- tests/steps/openpage.steps.js
+++ tests/steps/openpage.steps.js
@@ -3,15 +3,21 @@ const{Given,When,Then,And,Before,After,SetDefaultTimeout}=require("@cucumber/cuc
 const{ chromium,expect}=require("@playwright/test");
 const{page}=require("playwright")
 
+let browser;
+let page;
 
        
          Given('browser is open', function () {
+          browser=await chromium.launch({headless:false});
+          const context=await browser.newContext();
+          page=await context.newPage();
            // Write code here that turns the phrase above into concrete actions
            return 'pending';
          });
        
    
          Given('user is on google search page', function () {
+          await page.goto('https://www.google.com');
            // Write code here that turns the phrase above into concrete actions
            return 'pending';
          });
@@ -19,6 +25,8 @@ const{page}=require("playwright")
    
        
          When('user enters a text in search box', function () {
+          await page.fill('textarea[name="q"],'playwright testing');
+            await page.keyboard.press('enter');
            // Write code here that turns the phrase above into concrete actions
            return 'pending';
          });
